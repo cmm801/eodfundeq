9559fbad56e4ca7b968febdfa12903da052b0b3b
cmm801 <cmm801@gmail.com>
2023-04-11 15:45:22 +0200

    Add functions for calculating rolling mean/std and nDCG to utils.

--- eodfundeq/utils.py
+++ eodfundeq/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import warnings
 
 from eodfundeq.constants import ReturnTypes
@@ -31,19 +32,14 @@ def rolling_sum(arr, n_periods, min_obs=None, fillna=True):
 
     cs = np.nancumsum(arr, axis=0)
     roll_sum = np.vstack([
-            np.nan * np.ones((
-            n_periods-1, arr.shape[1]), dtype=arr.dtype),
-            cs[n_periods-1, :],
+            cs[:n_periods, :],
             cs[n_periods:, :] - cs[:arr.shape[0]-n_periods, :]
-    ])
+    ]).astype(np.float32)
     
-    observations = np.ones_like(cs, dtype=int) 
-    observations[np.isnan(arr)] = 0
-    cs_obs_window = np.nancumsum(observations, axis=0)
+    observations = ~np.isnan(arr)
+    cs_obs_window = np.cumsum(observations, axis=0)
     obs_in_window = np.vstack([
-            np.nan * np.ones((
-            n_periods-1, arr.shape[1]), dtype=arr.dtype),
-            cs_obs_window[n_periods-1, :],
+            cs_obs_window[:n_periods, :],
             cs_obs_window[n_periods:, :] - cs_obs_window[:arr.shape[0]-n_periods, :]
     ])
     roll_sum[obs_in_window < min_obs] = np.nan
@@ -52,6 +48,21 @@ def rolling_sum(arr, n_periods, min_obs=None, fillna=True):
     if not fillna:
         roll_sum[np.isnan(arr)] = np.nan
     return roll_sum
+
+def rolling_mean(arr, n_periods, min_obs=None, fillna=True):
+    rsum = rolling_sum(arr, n_periods, min_obs=min_obs, fillna=fillna)
+    n_obs = rolling_sum(~np.isnan(arr), n_periods, min_obs=1, fillna=fillna)
+    return rsum / n_obs
+
+def rolling_std(arr, n_periods, min_obs=None, fillna=True):
+    """Calculate the rolling standard deviation.
+
+    This should match the numpy calculation:
+        std = np.sqrt(np.mean(x - np.mean(x)))
+    """
+    mu = rolling_mean(arr, n_periods, min_obs=min_obs)
+    return np.sqrt(rolling_mean(arr ** 2, n_periods, min_obs=min_obs) - \
+                   2 * mu * rolling_mean(arr, n_periods, min_obs=min_obs) + mu ** 2)
 
 def rolling_return(arr, n_periods, return_type=None, fillna=True, tol=1e-6):
     """Compute a rolling return of a numpy array."""
@@ -81,4 +92,106 @@ def calc_feature_percentiles(array):
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore', r'All-NaN (slice|axis) encountered')
         percentiles = ranks / np.nanmax(ranks, axis=1, keepdims=True)
-    return percentiles    
+    return percentiles
+
+def ndcg(y_true, y_score, k=None, form='exp'):
+    df = pd.DataFrame(np.vstack([y_score, y_true]).T, columns=['score', 'true'])
+    df.sort_values('score', ascending=False, inplace=True)
+    if k is None:
+        k = df.shape[0]
+    k = min(k, df.shape[0])
+    relevance = df.true.values[:k]
+    ideal_rel = df.sort_values('true', ascending=False).true.values[:k]
+    discount = np.log2(np.arange(1, k+1) + 1)
+    if form in ('exp', 'exponential'):
+        DCG = (2 ** relevance - 1) / discount
+        IDCG = (2 ** ideal_rel - 1) / discount
+    else:
+        DCG = relevance / discount
+        IDCG = ideal_rel / discount
+
+    return DCG.sum() / IDCG.sum()    
+
+def cross_validate_gbm(model_handle, ds_helper, n_folds=5, **kwargs):
+    X = ds_helper.X['train']
+    y = ds_helper.y_class_norm['train']    
+    year_months = np.array(ds_helper.year_month['train'])  # ensure this is a numpy array
+    assert np.all(np.sort(year_months) == year_months), 'Samples should be sorted by year-month'    
+    n_buckets = len(set(y))
+    
+    uniq_year_months = sorted(set(year_months))
+    idx_bins = [int(x) for x in np.linspace(0, len(uniq_year_months), num=n_folds+1)]
+    bins = np.array([uniq_year_months[idx] for idx in idx_bins[:-1]], dtype=int)
+    idx_fold = np.digitize(year_months, bins) - 1
+
+    df_ym = pd.DataFrame(year_months, columns=['year_month'])
+    scores = []
+    models = []
+    for fold in range(n_folds):
+        model = model_handle(**kwargs)
+        group = df_ym.loc[idx_fold != fold].reset_index().groupby('year_month').count().to_numpy()
+        eval_yms = sorted(set(year_months[idx_fold == fold]))
+        eval_set = []
+        eval_group = []
+        for ym in eval_yms:
+            eval_set.append((X[year_months == ym,:], y[year_months == ym]))
+            eval_group.append([eval_set[-1][1].size])
+        Xtrain = X[idx_fold != fold,:]
+        ytrain = y[idx_fold != fold]
+        model.fit(Xtrain, ytrain, eval_at=[100],
+                  group=group, eval_group=eval_group,
+                  feature_name=ds_helper.feature_names,
+                  eval_set=eval_set)
+
+        fold_scores = []
+        for z in eval_set:
+            Xtest, ytest = z
+            y_score = model.predict(Xtest)
+            df = pd.DataFrame(np.vstack([y_score, ytest]).T, columns=['pred', 'true']).sort_values('pred', ascending=False)            
+            fold_scores.append(ndcg(df.true.values, df.pred.values, k=100, form='exp'))
+        scores.append(fold_scores)
+        models.append(model)
+
+    return scores, models
+
+
+def get_cv_grid_args(params_grid):
+    params_list = [dict()]
+    for key, vals in params_grid.items():
+        new_params_list = []
+        for params in params_list:
+            for val in vals:
+                p = params.copy()
+                p[key] = val
+                new_params_list.append(p)
+        params_list = new_params_list
+
+    return params_list
+
+def calc_cta_momentum_signals(ts_prices):
+    """Calculate the intermediate CTA Momentum signals following Baz et al.
+    
+    This function calculates the 16 intermediate momentum signals, following
+    the paper 'Dissecting Investment Strategies in the Cross Section and Time Series'
+    by Baz et al, published in 2015.
+    https://www.cmegroup.com/education/files/dissecting-investment-strategies-in-the-cross-section-and-time-series.pdf
+    """
+    S = [8, 16, 32]
+    L = [24, 48, 96]
+
+    HL = lambda n : np.log(0.5) / np.log(1 - 1/n)
+
+    signals = []
+    signal_names = []
+    for j in range(3):
+        x = ts_prices.ewm(halflife=HL(S[j])).mean() - ts_prices.ewm(halflife=HL(L[j])).mean()
+        y = x / ts_prices.rolling(63).std()
+        z = y / y.rolling(252).std()
+        u = x * np.exp(-np.power(x, 2) / 4) / 0.89    
+        signals.extend([x, y, z, u])
+        signal_names.extend([n + str(j) for n in ['x', 'y', 'z', 'u']])
+
+    df_signals_daily = pd.concat(signals, axis=1)
+    df_signals_daily.index = pd.DatetimeIndex(df_signals_daily.index)
+    df_signals_daily.columns = signal_names
+    return df_signals_daily.resample('M').last()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,6 @@ import eodfundeq.utils
 class UtilsTest(unittest.TestCase):
     def test_ffill(self):
         """Test that ffill is working as expected in a standard use case."""
-        print(f"\nRunning test method {self._testMethodName}\n")
         arr = np.array([
             [1, 2, np.nan],
             [3, np.nan, 4], 
@@ -39,7 +38,7 @@ class UtilsTest(unittest.TestCase):
             [30, 35, 40],
         ], dtype=float)
 
-        actual_rolling_sum = np.array([
+        true_rolling_sum = np.array([
             [np.nan, np.nan, np.nan],
             [np.nan, np.nan, np.nan],
             [12., 18., 24.],
@@ -48,7 +47,7 @@ class UtilsTest(unittest.TestCase):
         ])
 
         self.assertTrue(np.allclose(eodfundeq.utils.rolling_sum(arr, n_periods),
-                        actual_rolling_sum, 
+                        true_rolling_sum, 
                         equal_nan=True))
 
     def test_rolling_sum_min_obs(self):
@@ -62,24 +61,107 @@ class UtilsTest(unittest.TestCase):
             [30, 35, 40],
         ], dtype=float)
 
-        actual_rolling_sum = np.array([
+        true_rolling_sum = np.array([
             [np.nan, np.nan, np.nan],
             [np.nan, np.nan, np.nan],
             [12., 18., 17.],
             [30., 39., 41.],
             [57., 69., 81.],
         ])
-
+        
         with self.subTest(min_obs=2):
+            true_rolling_sum_2 = true_rolling_sum.copy()
+            true_rolling_sum_2[1,:] = np.sum(arr[:2,:], axis=0)
             self.assertTrue(np.allclose(eodfundeq.utils.rolling_sum(arr, n_periods, min_obs=2),
-                            actual_rolling_sum, 
+                            true_rolling_sum_2, 
                             equal_nan=True))
+        
+        with self.subTest(min_obs=1):
+            true_rolling_sum_1 = true_rolling_sum.copy()
+            true_rolling_sum_1[0,:] = arr[0,:]
+            true_rolling_sum_1[1,:] = np.nansum(arr[:2,:], axis=0)
+            self.assertTrue(np.allclose(eodfundeq.utils.rolling_sum(arr, n_periods, min_obs=1),
+                            true_rolling_sum_1, 
+                            equal_nan=True))                            
 
-        actual_rolling_sum[1:4, 2] = np.nan
+        true_rolling_sum[1:4, 2] = np.nan
         with self.subTest(min_obs=3):
             self.assertTrue(np.allclose(eodfundeq.utils.rolling_sum(arr, n_periods, min_obs=3),
-                            actual_rolling_sum, 
+                            true_rolling_sum, 
                             equal_nan=True))
+
+    def test_rolling_mean_basic(self):
+        n_periods = 3
+        arr = np.array([
+            [ 0,  1,  2],
+            [ 3,  5,  7],
+            [ 9, 12, 15],
+            [18, 22, 26],
+            [30, 35, 40],
+        ], dtype=float)
+
+        true_rolling_mean = np.array([
+            [np.nan, np.nan, np.nan],
+            [np.nan, np.nan, np.nan],
+            [4., 6., 8.],
+            [10., 13., 16.],
+            [19., 23., 27.],
+        ])
+        self.assertTrue(np.allclose(eodfundeq.utils.rolling_mean(arr, n_periods),
+                        true_rolling_mean, 
+                        equal_nan=True))
+
+    def test_rolling_mean_min_obs(self):
+        n_periods = 3
+        min_obs = 2
+        arr = np.array([
+            [ 0,  1,  2],
+            [ 3,  np.nan,  7],
+            [ 9, 12, 15],
+            [18, 22, 26],
+            [30, 35, np.nan],
+        ], dtype=float)
+
+        true_rolling_mean = np.array([
+            [np.nan, np.nan, np.nan],
+            [3/2, np.nan, 9/2],
+            [4., 13/2, 8.],
+            [10., 34/2., 16.],
+            [19., 23., 41/2],
+        ])
+        self.assertTrue(np.allclose(eodfundeq.utils.rolling_mean(arr, n_periods, min_obs=min_obs),
+                        true_rolling_mean, 
+                        equal_nan=True))
+
+    def test_rolling_std_basic(self):
+        n_periods = 3
+        min_obs = 2
+        arr = np.array([
+            [ 0,  1,  2],
+            [ 3,  5,  np.nan],
+            [ 9, 12, 15],
+            [18, 22, 26],
+            [30, 35, 40],
+            [32, 13, 24],    
+        ], dtype=float)
+
+        true_rolling_std = np.nan * np.ones_like(arr)
+        for col in range(arr.shape[1]):
+            j = 0
+            while j < arr.shape[0]:
+                if j < min_obs - 1:
+                    j += 1
+                    continue
+                
+                s = slice(max(0, j - n_periods + 1), j+1)
+                if (~np.isnan(arr[s, col])).sum() >= min_obs:
+                    true_rolling_std[j, col] = np.nanstd(arr[s, col])
+                j += 1
+
+        self.assertTrue(np.allclose(eodfundeq.utils.rolling_std(arr, n_periods, min_obs=min_obs),
+                        true_rolling_std, 
+                        equal_nan=True))
+
 
 
 if __name__ == '__main__':
