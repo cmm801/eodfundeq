f153e7af90234aa3d9eeaac54c4ab89dd2c5d0ac
cmm801 <cmm801@gmail.com>
2023-04-26 16:57:36 +0200

    Update performance calculation to use "secdb" package.

--- eodfundeq/scoring.py
+++ eodfundeq/scoring.py
@@ -8,13 +8,22 @@ from pypfopt import HRPOpt
 from typing import Optional
 
 from pyfintools.tools import optim
+from secdb.strategy.core import RebalanceByWeights
 
-from eodfundeq.constants import START_DATE, DatasetTypes, ModelTypes
-from eodfundeq import filters
+from eodfundeq.constants import ModelTypes, ReturnTypes
 from eodfundeq import utils
 
 
-def get_selected_returns(model, dataset, daily_prices, n_stocks=100, price_tol=1e-6):
+def get_selected_log_returns(model, dataset, daily_prices, n_stocks=100, price_tol=1e-6):
+    return get_selected_returns(
+        model, dataset, daily_prices, return_type=ReturnTypes.LOG, n_stocks=n_stocks, price_tol=price_tol)
+
+def get_selected_arith_returns(model, dataset, daily_prices, n_stocks=100, price_tol=1e-6):
+    return get_selected_returns(
+        model, dataset, daily_prices, return_type=ReturnTypes.ARITHMETIC, n_stocks=n_stocks, price_tol=price_tol)
+
+def get_selected_returns(model, dataset, daily_prices, return_type, n_stocks=100, price_tol=1e-6):
+    assert return_type in ReturnTypes
     timestamps = dataset.timestamp
     true_rtns_list = []
     selected_symbols_list = []
@@ -32,8 +41,12 @@ def get_selected_returns(model, dataset, daily_prices, n_stocks=100, price_tol=1
         period_start = period_end - pd.Timedelta(91, unit='d')
         idx_period = (period_start <= daily_prices.index) & (daily_prices.index <= period_end)
         period_prices = daily_prices.loc[idx_period, selected_symbols]
-        rtns = np.log(np.maximum(period_prices, price_tol) / \
-                    np.maximum(period_prices.shift(1), price_tol))
+        price_ratio = np.maximum(period_prices, price_tol) / \
+                      np.maximum(period_prices.shift(1), price_tol)
+        if return_type == ReturnTypes.LOG:
+            rtns = np.log(price_ratio)
+        else:
+            rtns = -1 + price_ratio
         rtns = rtns.iloc[1:,:]
         assert np.isnan(rtns.values).sum(axis=0).max() < 10
         true_rtns = pd.Series(dataset.true_return.loc[idx_t].values, index=symbols_t)
@@ -45,7 +58,7 @@ def get_selected_returns(model, dataset, daily_prices, n_stocks=100, price_tol=1
     return df_rtns, df_symbols
 
 def get_strategy_weights(model, dataset, daily_prices, target_vol=0.15, n_stocks=100, weighting='equal'):
-    _, df_symbols = get_selected_returns(model, dataset, daily_prices, n_stocks=n_stocks)
+    _, df_symbols = get_selected_arith_returns(model, dataset, daily_prices, n_stocks=n_stocks)
     weights_list = []
     for period_end in df_symbols.index:
         if weighting == 'equal':
@@ -74,7 +87,7 @@ def _calculate_covariance(daily_prices, period_end, symbols, n_days=91, price_to
     idx_period = (period_start <= daily_prices.index) & (daily_prices.index <= period_end)
     period_prices = daily_prices.loc[idx_period, symbols]
     rtns = np.log(np.maximum(period_prices, price_tol) / \
-                np.maximum(period_prices.shift(1), price_tol))
+                  np.maximum(period_prices.shift(1), price_tol))
     rtns = rtns.iloc[1:,:]
     assert np.isnan(rtns.values).sum(axis=0).max() < 10
 
@@ -85,32 +98,45 @@ def _calculate_covariance(daily_prices, period_end, symbols, n_days=91, price_to
     asset_cov = cov_nearest(asset_cov_non_pos_def, threshold=1e-6)
     return asset_cov
 
-def get_performance(model, dataset, daily_prices, n_stocks=100, weighting='equal'):
-    df_rtns, _ = get_selected_returns(model, dataset, daily_prices, n_stocks=n_stocks)
-    df_wts = get_strategy_weights(model, dataset, daily_prices, n_stocks=n_stocks, weighting=weighting)
-    realized_rtns = []
-    for period_end in df_rtns.index:
-        weights = df_wts.loc[period_end].values
-        true_rtns = df_rtns.loc[period_end].values
-        realized_rtns.append((weights * true_rtns).sum())
-    return pd.Series(realized_rtns, index=df_rtns.index)
+def get_performance(model, dataset, daily_prices, return_window, 
+                    n_stocks=100, weighting='equal'):
+    _, df_symbols = get_selected_log_returns(
+        model, dataset, daily_prices, n_stocks=n_stocks)
+    target_wts = get_strategy_weights(model, dataset, daily_prices, weighting=weighting)
+    df_weights = get_portfolio_weights(target_wts, df_symbols, return_window)
+    
+    # We exclude stocks from selection if they have low or NaN prices.
+    # However, a stock that is selected could have its price become NaN
+    # during the investment period. In this case, we just use the last
+    # available price until the end of the period.
+    df_prices = daily_prices[df_weights.columns.values].ffill()
+        
+    # Align dates of weights with pricing dates
+    idx_dt = df_prices.index.get_indexer(df_weights.index, 'ffill')
+    df_weights.index = df_prices.index[idx_dt]
 
-def calc_pro_rata_performance(rtns, return_window):
-    dti = pd.DatetimeIndex(rtns.index)
-    start = rtns.index[0] - pd.tseries.offsets.MonthEnd(1)
-    strat_perf = pd.DataFrame(np.nan * np.ones((rtns.shape[0]+1, return_window), dtype=float), 
-                            index=dti.append(pd.DatetimeIndex([start])).sort_values())
-    strat_perf.iloc[0] = 1/return_window
-    for j in range(rtns.shape[0]):
-        for col in range(return_window):
-            if col == j % return_window:
-                strat_perf.iloc[j+1, col] = strat_perf.iloc[j, col] * (1 + rtns.iloc[j])
+    ptf = RebalanceByWeights(weights=df_weights, prices=df_prices)
+    ptf.initial_value = 1.0
+    mv_ts, _ = ptf.calc_performance(
+        start=df_weights.index[0],
+        end=df_weights.index[-1] + pd.tseries.offsets.MonthEnd(return_window))
+    return mv_ts
 
-            else:
-                strat_perf.iloc[j+1, col] = strat_perf.iloc[j, col]
-    total_perf = strat_perf.sum(axis=1)
-    total_perf.name = rtns.name
-    return total_perf
+def get_portfolio_weights(target_wts, df_symbols, return_window):
+    uniq_symbols = sorted(set(df_symbols.values.flatten()))
+    dates = list(df_symbols.index.values)
+    for j in range(1, return_window):
+        dates.append(dates[-1] + pd.tseries.offsets.MonthEnd(1))
+
+    wts_init_arr = np.zeros((df_symbols.shape[0] + return_window - 1,
+                             len(uniq_symbols)), dtype=np.float32)
+    df_weights = pd.DataFrame(wts_init_arr, index=dates, columns=uniq_symbols)
+    for idx in target_wts.index:
+        for j in range(return_window):
+            idx_t = idx + pd.tseries.offsets.MonthEnd(j)
+            df_weights.loc[idx_t, df_symbols.loc[idx]] += target_wts.loc[idx].values
+
+    return df_weights / df_weights.sum(axis=1).values.reshape(-1, 1)
 
 def get_ndcg(model, dataset, n_buckets, n_stocks=100):
     timestamps = dataset.timestamp
